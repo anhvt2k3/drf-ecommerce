3515548072d9eb682d51dabe6db3b397eecaa782
anhvt2k3 <anh.vantanh@hcmut.edu.vn>
2024-10-16 15:51:51 +0700

    Minor fixings with more tasks on

--- webhook/views.py
+++ webhook/views.py
@@ -34,29 +34,28 @@ class PaymentStripeWebhookView(generics.GenericAPIView):
             data = ViewUtils.gen_response(success=False, status=HTTP_400_BAD_REQUEST, message='Invalid signature.', data=str(e))
             return Response(data, data['status'])
         
-
         # Handle the event
-        if event['type'] == 'checkout.session.completed':
-            return self.handle_successful_payment(event)
+        if event.type == 'checkout.session.completed':
+            return self.handle_successful_payment(event.data.object)
 
-        elif event['type'] == 'payment_intent.payment_failed':
-            return self.handle_failed_payment(event)
+        elif event.type == 'payment_intent.payment_failed':
+            return self.handle_failed_payment(event.data.object)
         
-        elif event['type'] == 'customer.created':
+        elif event.type == 'customer.created':
             pass
         
-        elif event['type'] == 'invoice.created':
-            return self.handle_invoice_creation(event)
+        elif event.type == 'invoice.created':
+            return self.handle_invoice_creation(event.data.object)
         
-        elif event['type'] == 'customer.subscription.created':
-            return self.handle_subscription_creation(event)
+        elif event.type == 'customer.subscription.created':
+            return self.handle_subscription_creation(event.data.object)
         
         data = ViewUtils.gen_response(success=True, status=HTTP_200_OK, message='Invalid event type.', data=event['type'])
         return Response(data, status=data['status'])
 
-    def handle_subscription_creation(event):
+    def handle_subscription_creation(self, event):
         # print ('SUBSCRIPTION UPDATE')
-        stripeSubscription = event['data']['object']
+        stripeSubscription = event
         subscriptionMetadata = stripeSubscription['metadata']
         localSubscription = Subscription.objects.filter(id=subscriptionMetadata['subscription']).first()
         if not localSubscription.stripeSubscriptionID:
@@ -65,9 +64,10 @@ class PaymentStripeWebhookView(generics.GenericAPIView):
         data = ViewUtils.gen_response(success=True, status=HTTP_200_OK, message='Subscription creation acknowledged.', data=stripeSubscription)
         return Response(data, status=data['status'])
 
-    def handle_invoice_creation(event):
+    def handle_invoice_creation(self, event):
         # print ('INVOICE UPDATE')
-        stripeInvoice = event['data']['object']
+        #: branch condition -> update subscription pay status
+        stripeInvoice = event
         subscriptionMetadata = stripe.Subscription.retrieve(stripeInvoice['subscription'])['metadata']
         if not Subscription.objects.filter(receipt__id=stripeInvoice['id']).exists():
             serializer = InvoiceSerializer(
@@ -81,27 +81,28 @@ class PaymentStripeWebhookView(generics.GenericAPIView):
         data = ViewUtils.gen_response(success=True, status=HTTP_200_OK, message='Invoice creation acknowledged.', data=stripeInvoice)
         return Response(data, status=data['status'])
     
-    def handle_successful_payment(event):
+    def handle_successful_payment(self, event):
         """
         Handle the successful payment logic here.
         """
         #: implement duplicate event check
+        #: branch condition -> update subscription pay status
         # print ('PAYMENT UPDATE')
-        session = event['data']['object']
-        session = session['data']['object']  # Checkout Session object
+        session = event
+        session = session  # Checkout Session object
         order_id = session['metadata']['order_id']
         OrderSerializer().update(Order.objects.filter(id=order_id).first(), {'status':'processing'})
         data = ViewUtils.gen_response(success=True, status=HTTP_200_OK, message='Payment successful.', data=session)
         return Response(data, status=data['status'])
             
 
-    def handle_failed_payment(event):
+    def handle_failed_payment(self, event):
         """
         Handle the failed payment logic here.
         """
         #: implement duplicate event check
         # print ('PAYMENT UPDATE')
-        payment_intent = event['data']['object']
+        payment_intent = event
         data = ViewUtils.gen_response(success=True, status=HTTP_200_OK, message='Payment failed. Forget something in your purchase?', data=payment_intent)
         return Response(data, status=data['status'])
             
